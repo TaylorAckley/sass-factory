579f69de35f2cdbec3e1fad67f2dd51e75349cc7
Taylor Ackley <taylorackley@gmail.com>
2017-03-04 12:37:05 -0800

    travis commit

--- tests/base64.spec.js
+++ tests/base64.spec.js
@@ -8,6 +8,7 @@ let base64 = require('../lib/base64.module');
 let ts = moment().format('YYY_MM_DD');
 let _file = './tests/test.scss';
 let _str = fs.readFileSync(_file, 'utf-8');
+console.log(_str);
 let _check = 'JGNvbG9yMzogcmVkOw0KDQpwIHsNCmNvbG9yOiAke2NvbG9yMX07DQp9DQoNCi5oZWFkaW5nIHsNCmZvbnQtc2l6ZTogMTJweDsNCmNvbG9yOiAke2NvbG9yMn07DQp9DQoNCi5yZWQgew0KICAgIGNvbG9yOiAkY29sb3IzOw0KfQ==';
 
 
@@ -22,6 +23,7 @@ describe('base64FromString', () => {
     });
     it('should return a base64 string', () => {
         let res = base64.base64FromString(_str);
+        console.log(res);
         res.should.equal(_check);
     });
 });
@@ -32,6 +34,7 @@ describe('base64FromFile', () => {
     });
     it('should return a base64 string', () => {
         let res = base64.base64FromFile(_file);
+        console.log(res);
         res.should.equal(_check);
     });
 });
